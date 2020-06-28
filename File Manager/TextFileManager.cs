0445311e1347536001d5166442aeb002bedfb9e1
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-06-28 14:30:18 +0100

    task/NAMES-177 - Original List now returns a list of names from names.txt | Added Extensions class | Added various bin and obj directories to .gitignore

---
+++ File Manager/TextFileManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Extensions;
+
+namespace File_Manager
+{
+    public class TextFileManipulator : IFileManager
+    {
+        public string GetRawFileData()
+        {
+            return File.ReadLines("names.txt").FirstOrDefault();
+        }
+
+        public List<string> ConvertRawDataToList()
+        {
+            string rawData = GetRawFileData();            
+            List<string> stringList = new List<string>();
+
+            string[] stringArray = rawData.Split(',');
+
+            foreach (var item in stringArray)
+            {
+                stringList.Add(item.RemoveQuotes());
+            }
+
+            return stringList;
+        }
+    }
+}
